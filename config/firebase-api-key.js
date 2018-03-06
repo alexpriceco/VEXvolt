ba020ca319b3ef87d4d5834b9bc4b3e131257f7f
Alex Price <alex@alexprice.co>
2018-03-06 17:25:03 -0600

    Abandon 3js, move to Next.js

---
+++ config/firebase-api-key.js
@@ -0,0 +1,2 @@
+const FIREBASE_API_KEY = 'AIzaSyCmX8fGVjdVVqtgEd74rcEHdFz34kh5d1k'
+module.exports = FIREBASE_API_KEY
---
+++ pages/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+
+import Stylesheet from '../components/stylesheet.js'
+import sheet from '../components/base.scss'
+
+import Child from '../components/child/child.js'
+
+// TODO: Uncomment if you want Firebase
+// import config from '../config/firebase-api-key.js'
+// import * as firebase from 'firebase'
+// require('firebase/firestore')
+// if (!firebase.apps.length) {
+//   console.log(
+//     '%cCreating a new firebase instance...',
+//     'color: grey; font-style: italic'
+//   )
+//
+//   firebase.initializeApp(config)
+// }
+
+export class Index extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      loading: true,
+      error: '',
+
+      // Data from Firebase
+      data: {}
+    }
+  }
+
+  componentDidMount () {
+    console.debug('Loaded')
+    this.setState({ loading: false })
+
+  // TODO: uncomment if you want Firebase
+  //   this.getDataFromFirebase().then((data) => {
+  //     this.setState({
+  //       loading: false,
+  //       data
+  //     })
+  //   }).catch((error) => {
+  //     this.setState({
+  //       loading: false,
+  //       error
+  //     })
+  //   })
+  }
+
+  // TODO: uncomment if you want Firebase
+  // async getDataFromFirebase () {
+  //   const rootCollection = firebase.firestore().collection('triplebyte')
+  //   const rootDoc = await rootCollection.doc('root').get()
+  //   return rootDoc.data()
+  // }
+
+  render () {
+    if (this.state.loading) {
+      return (
+        <main>
+          Just a second...
+          <Stylesheet sheet={sheet} />
+        </main>
+      )
+    } else if (this.state.error) {
+      return (
+        <main>
+          <h1>That's bad. The following error occurred:</h1>
+          <div className='error'>{this.state.error}</div>
+          <Stylesheet sheet={sheet} />
+        </main>
+      )
+    }
+
+    return (
+      <main>
+        <h1>App title</h1>
+        <Child data={this.state.data} />
+        <Stylesheet sheet={sheet} />
+      </main>
+    )
+  }
+}
+
+export default Index
